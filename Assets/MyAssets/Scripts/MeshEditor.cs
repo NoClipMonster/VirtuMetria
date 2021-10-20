608284d75d6b60f149b9d275388203a988e753e1
VladislavKudaktin <iavedis9@gmail.com>
2021-10-20 17:58:55 +0300

    Изменил класс Dot

--- Assets/MyAssets/Scripts/MeshEditor.cs
+++ Assets/MyAssets/Scripts/MeshEditor.cs
@@ -1,24 +1,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 public class MeshEditor : MonoBehaviour
 {
     Mesh oMesh;
     MeshFilter oMeshFilter;
     MeshCollider oMeshCollider;
     int[] triangles;
+    public GameObject layoutDot;
 
     List<List<int>> SimilarIndexes = new List<List<int>>();
     public Vector3[] Vertices;
-    public Dots dots = new Dots();
+    public Dots dots;
     public class Dots
     {
         public List<Dot> dots = new List<Dot>();
-        public class Dot
+        public GameObject LayoutDot;
+        public class Dot : Dots
         {
-            public Vector3 vector3;
-            public int[] similarDots;
+            Vector3 vector3;
+            public Vector3 Vector3
+            {
+                get { return vector3; }
+                set
+                {
+                    vector3 = value;
+                }
+            }
+            public List<int> similarDots = new List<int>();
+            public bool showDot = false;
+            public new GameObject LayoutDot;
+        }
+
+        public void ShowDot(int index, MeshFilter mf)
+        {
+            Dot dot = dots[index];
+            dot.showDot = true;
+            dot.LayoutDot = LayoutDot;
+            Instantiate(dot.LayoutDot, mf.gameObject.transform.position + dot.Vector3, new Quaternion(0, 0, 0, 0));
         }
         public Dot GetDots(int index)
         {
@@ -28,35 +47,34 @@ public class MeshEditor : MonoBehaviour
         {
             foreach (var item in dots)
             {
-                if (item.vector3 == vector3)
+                if (item.Vector3 == vector3)
                     return item;
-                
             }
             return null;
         }
+
     }
-  
-   
+
     void FillDot()
     {
         for (int i = 0; i < SimilarIndexes.Count; i++)
         {
-            dots.dots.Add(new Dots.Dot { vector3 = Vertices[SimilarIndexes[i][0]], similarDots = SimilarIndexes[i].ToArray() });
+            dots.dots.Add(new Dots.Dot { Vector3 = Vertices[SimilarIndexes[i][0]], similarDots = SimilarIndexes[i] });
         }
     }
     void ShowDot()
     {
         foreach (var dot in dots.dots)
         {
-            Debug.Log(getVector(dot.vector3)+ "  :::Точки с данным вектором: "+ getDots(dot.similarDots));
+            Debug.Log(getVector(dot.Vector3) + "  :::Точки с данным вектором: " + getDots(dot.similarDots));
         }
         string getVector(Vector3 v)
         {
-            string str = v.x + " " + v.y+" " + v.z;
-         
+            string str = v.x + " " + v.y + " " + v.z;
+
             return str;
         }
-        string getDots(int[] dots)
+        string getDots(List<int> dots)
         {
             string str = "";
             foreach (var item in dots)
@@ -70,84 +88,65 @@ public class MeshEditor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bool[] visited;
-        List<Vector3> buf = new List<Vector3>();
+
+        dots = new Dots();
+        dots.LayoutDot = layoutDot;
 
         oMeshFilter = GetComponent<MeshFilter>();
         oMeshCollider = GetComponent<MeshCollider>();
+        oMesh = oMeshFilter.sharedMesh;
 
-        oMesh = oMeshFilter.sharedMesh; //1
+        Vertices = oMesh.vertices;
 
-        Vertices = oMesh.vertices; //4
-        triangles = oMesh.triangles;
-        Debug.Log("Init & Cloned");
+        bool[] visited = new bool[Vertices.Length];
 
         for (int i = 0; i < Vertices.Length; i++)
-        {
-            buf.Add(Vertices[i]);
-        }
-
-        visited = new bool[Vertices.Length];
-
-        for (int i = 0; i < buf.Count; i++)
         {
             if (visited[i])
             {
                 continue;
             }
-
-            SimilarIndexes.Add(new List<int>());
-            for (int j = i; j < buf.Count; j++)
+            dots.dots.Add(new Dots.Dot() { Vector3 = Vertices[i], similarDots = new List<int>() });
+            for (int j = 0; j < Vertices.Length; j++)
             {
-                if (buf[i] == buf[j])
+                if (Vertices[i] == Vertices[j])
                 {
-                    SimilarIndexes[i].Add(j);
+                    dots.dots[i].similarDots.Add(j);
                     visited[j] = true;
                 }
             }
         }
-        int h = 0;
-        Vertices = new Vector3[SimilarIndexes.Count];
-        foreach (var item in SimilarIndexes)
-        {
-
-            Vertices[h] = oMesh.vertices[item[0]];
-            h++;
-            string str = "";
-            foreach (var item2 in item)
-            {
-                str += item2 + " ";
-            }
-            Debug.Log(str);
-
-        }
-
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Keyboard.current.eKey.wasPressedThisFrame)
-        { FillDot();
+        {
+            FillDot();
             ShowDot();
+            for (int i = 0; i < dots.dots.Count; i++)
+            {
+                dots.ShowDot(i, oMeshFilter);
+            }
         }
-            if (Keyboard.current.spaceKey.isPressed)
+        if (Keyboard.current.spaceKey.isPressed)
         {
             Vector3[] vectors = oMeshFilter.mesh.vertices;
 
-            foreach (var item in SimilarIndexes)
+            foreach (var item in dots.dots)
             {
-                if (vectors[item[0]].y < 0 && vectors[item[0]].x < 0)
+                if (item.Vector3.y < 0 && item.Vector3.x < 0)
                 {
-                    foreach (var index in item)
+                    item.Vector3 += Vector3.one / 100;
+                    foreach (var item1 in item.similarDots)
                     {
-                        vectors[index] -= Vector3.up / 100;
+                        oMeshFilter.mesh.vertices[item1] = item.Vector3;
                     }
-
+                    
                 }
             }
-
-            oMeshFilter.mesh.vertices = vectors;
+            
             oMeshFilter.mesh.RecalculateNormals();
             oMeshFilter.mesh.RecalculateBounds();
             oMeshFilter.mesh.RecalculateTangents();
---
+++ Assets/MyAssets/Scripts/RigMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RigMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+       // d = InputSystem.GetDevice("");
+    }
+    InputDevice d;
+    // Update is called once per frame
+    void Update()
+    {
+
+       // transform.position(InputSystem.GetDevice());
+    }
+}
