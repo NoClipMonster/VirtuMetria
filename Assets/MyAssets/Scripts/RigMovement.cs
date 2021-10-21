bdfceebababf7513ecfedfb35fb282766f8709f0
VladislavKudaktin <iavedis9@gmail.com>
2021-10-21 10:39:49 +0300

    Поставил SteamVR

--- Assets/MyAssets/Scripts/RigMovement.cs
+++ Assets/MyAssets/Scripts/RigMovement.cs
@@ -1,21 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using Valve.VR;
+
 
 public class RigMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
- 
-       // d = InputSystem.GetDevice("");
-    }
-    InputDevice d;
-    // Update is called once per frame
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean telepotrAction;
+    public SteamVR_Action_Boolean grabAction;
+
     void Update()
     {
+        if (GetTeleportDown())
+        {
+            print("Teleport " + handType);
+        }
 
-       // transform.position(InputSystem.GetDevice());
+        if (GetGrab())
+        {
+            print("Grab " + handType);
+        }
+    }
+    public bool GetTeleportDown()
+    {
+        return telepotrAction.GetStateDown(handType);
+    }
+    public bool GetGrab()
+    {
+        return grabAction.GetState(handType);
     }
 }
